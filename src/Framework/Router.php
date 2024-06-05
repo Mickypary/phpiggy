7f22c29f5831eafa6171eb540924a1364724a588
Mickypary <mikipary@gmail.com>
2024-06-05 16:02:21 +0100

    another commit

--- src/Framework/Router.php
+++ src/Framework/Router.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 
 namespace Framework;
 
-class Router {
+class Router
+{
 
     private array $routes = [];
 
-    public function add(string $method, string $path, array $controller) {
+    public function add(string $method, string $path, array $controller)
+    {
         $path = $this->normalizePath($path);
         $this->routes[] = [
             'path' => $path,
@@ -17,17 +19,19 @@ class Router {
         ];
     }
 
-    private function normalizePath(string $path) : string {
+    private function normalizePath(string $path): string
+    {
         $path = trim($path, '/');
         $path = "/{$path}/";
-        $path = preg_replace('#[/]{2,}#','/',$path);
+        $path = preg_replace('#[/]{2,}#', '/', $path);
         return $path;
     }
 
-    public function dispatch(string $path, string $method) {
+    public function dispatch(string $path, string $method)
+    {
         $path = $this->normalizePath($path);
         $method = strtoupper($method);
-        
+
         foreach ($this->routes as $route) {
             if (
                 !preg_match("#^{$route['path']}$#", $path) ||
@@ -39,7 +43,10 @@ class Router {
             // the below is called destructuring Array and assigning them to a variable, i.e class name first and method second in this case.
             // the order does matter
             [$class, $function] = $route['controller'];
+
+            $controllerInstance = new $class;
+
+            $controllerInstance->{$function}();
         }
     }
 }
-
